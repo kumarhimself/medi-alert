3f89623f66280c83950b9e00ee45af610c322af7
Ayush Kumar <ayush.k@outlook.com>
2024-07-18 22:49:28 -0400

    Created homepage, dashboard page, and updated README.md to include tasks tbd.

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Homepage!
+      Dashboard
     </main>
   );
 }
