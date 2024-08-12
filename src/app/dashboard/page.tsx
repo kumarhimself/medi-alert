38e8dcd2beeaf5736f389b15ce5f5bec71649b51
Ayush Kumar <ayush.k@outlook.com>
2024-08-11 23:57:17 -0400

    Added Gemini chatbot to dashboard along with API key in .env.local.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ai-chat-sidebar.tsx"; 
+import Button from "@/components/ui/button.tsx"; 
+import AIChat from "@/components/ai-chat-sidebar.tsx"; 
 
 export default function Dashboard() {
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">Dashboard</h1>
-      <Button text="Click me!" />
+    <main className="w-screen h-screen flex flex-row">
+      <div className="w-1/3 bg-red-100">
+        {/* Patient Menu */}
+      </div>
+      <div className="w-1/3 bg-green-100">
+        {/* Current Patient's EMR/EHR */}
+      </div>
+
+      <div className="w-1/3 h-screen justify-end bg-blue-100 overflow-auto">
+        <AIChat />
+      </div>
     </main>
-    
+
   );
 }
--- src/components/ai-chat-sidebar.tsx
+++ src/components/ai-chat-sidebar.tsx
@@ -6,23 +6,27 @@ export default function Chat() {
 
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
+  // fixed bottom-0 w-full flex justify-center mb-8
+
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="flex flex-col w-full max-w-md pt-10 pb-24">
       {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap my-2">
+        <div key={m.id} className="w-full whitespace-pre-wrap my-2 mx-5 bg-red-100">
           {m.role === 'user' ? 'User: ' : 'AI: '}
           {m.content}
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+      <div className="">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 mx-auto border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
     </div>
   );
 }
